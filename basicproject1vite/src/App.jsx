01a3de55fe5adb12c25c345855d0a2c89df96f56
Shreya Chatterjee <shreyachatterjee095@gmail.com>
2024-03-27 10:25:02 +0530

    comparing createreactapp and vite

---
+++ basicproject1vite/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Youtube from "./Youtube";
+
+function App() {
+  return (
+    <>
+   <h1>Vite React App</h1>
+   <h1>Vite React App</h1>
+   <Youtube/>
+   </>
+  )
+}
+
+export default App
